01972dfd0f773b7149bad699dc52e8d9cb1eaf97
Aakash69420 <aakashbhattacharjee73@gmail.com>
2023-03-06 16:22:27 +0530

    added login and signup pages

--- weather.js
+++ weather.js
@@ -1,36 +1,53 @@
 const api = {
   key: "4d570ebc6b10980b1cabb265d70ee4d1",
   base: "https://api.openweathermap.org/data/2.5/"
- }
-
- const searchBox = document.querySelector('.search-box');
- const searchBtn = document.querySelector('.search-btn');
- searchBtn.addEventListener('click', () => {
-   const query = searchBox.value;
-   if (query) {
-     getResults(query);
-   }
- });
-
- searchBox.addEventListener('keydown', (e) => {
-if (e.keyCode === 13) {
- e.preventDefault();
- const query = searchBox.value;
- if (query) {
-   getResults(query);
- }
 }
+
+const searchBox = document.querySelector('.search-box');
+const searchBtn = document.querySelector('.search-btn');
+
+searchBtn.addEventListener('click', () => {
+  const query = searchBox.value;
+  if (query) {
+    getWeatherData(query);
+  }
 });
 
+searchBox.addEventListener('keydown', (e) => {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    const query = searchBox.value;
+    if (query) {
+      getWeatherData(query);
+    }
+  }
+});
 
- function getResults(query) {
-  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-    .then(weather => {
-      return weather.json();
-    }).then(displayResults);
- }
+function getWeatherData(city) {
+  fetch(`${api.base}weather?q=${city}&units=metric&APPID=${api.key}`)
+    .then(response => {
+      if (!response.ok) {
+        alert("Not a valid city")
+        location.reload();
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.cod === "404") {
+        const errorMessage = document.querySelector('.error-message');
+        errorMessage.textContent = "City not found";
+        errorMessage.style.display = "block";
+      } else {
+        displayResults(data);
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
 
- function displayResults(weather) {
+function displayResults(weather) {
   let city = document.querySelector('.city');
   city.innerText = `${weather.name}, ${weather.sys.country}`;
 
@@ -47,7 +64,6 @@ if (e.keyCode === 13) {
   let hilow = document.querySelector('.hi-low');
   hilow.innerHTML = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
 
-  // Animation and Styling
   const appWrap = document.querySelector('.app-wrap');
   appWrap.classList.add('fade-in');
 
@@ -56,9 +72,9 @@ if (e.keyCode === 13) {
 
   const location = document.querySelector('.location');
   location.classList.add('fade-in');
- }
+}
 
- function dateBuilder(d) {
+function dateBuilder(d) {
   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
@@ -68,4 +84,12 @@ if (e.keyCode === 13) {
   let year = d.getFullYear();
 
   return `${day} ${date} ${month} ${year}`;
- }
+}
+
+document.getElementById("btn").addEventListener("click", function() {
+  // Redirect user to login page
+  window.location.href = "login.html";
+});
+
+
+
